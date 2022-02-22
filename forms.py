0f663e67c9d9a1602583ecf5c0049dc4c5abc542
OyugoObonyo <nyikal1999@gmail.com>
2022-02-22 21:01:11 +0300

    use flaskform to create upload and download forms

---
+++ forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileAllowed
+
+
+class UploadForm(FlaskForm):
+    """
+    Form that handles the upload file route
+    """
+    file = FileField('File', validators=[DataRequired(), FileAllowed(['pdf'])])
+    submit = SubmitField('Upload file')
+
+
+class DownloadForm(FlaskForm):
+    """
+    Form handling download file route
+    """
+    pass
