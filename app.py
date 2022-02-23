f30d8c508cade643c794b399628bda791937a20d
OyugoObonyo <nyikal1999@gmail.com>
2022-02-23 21:58:44 +0300

    Include secret key for form handling

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template
 import pyrebase
 from forms import DownloadForm, UploadForm
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
-firebaseConfig = {
-  "apiKey": "AIzaSyAgOVNgtr2mWRYRRxupHm0dXI5Pb0BwRjA",
-  "authDomain": "file-uploads-e52cd.firebaseapp.com",
-  "projectId": "file-uploads-e52cd",
-  "storageBucket": "file-uploads-e52cd.appspot.com",
-  "messagingSenderId": "59877063629",
-  "appId": "1:59877063629:web:e7840f23a8f0f805402284",
-  "measurementId": "G-6CRW0DT9V7"
-}
+load_dotenv('.env')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+# firebaseConfig = {
+#   "apiKey": "AIzaSyAgOVNgtr2mWRYRRxupHm0dXI5Pb0BwRjA",
+#   "authDomain": "file-uploads-e52cd.firebaseapp.com",
+#   "projectId": "file-uploads-e52cd",
+#   "storageBucket": "file-uploads-e52cd.appspot.com",
+#   "messagingSenderId": "59877063629",
+#   "appId": "1:59877063629:web:e7840f23a8f0f805402284",
+#   "measurementId": "G-6CRW0DT9V7"
+# }
 # initialize pyrebase app
-firebase = pyrebase.initialize_app(firebaseConfig)
-storage = firebase.storage()
+# firebase = pyrebase.initialize_app(firebaseConfig)
+# storage = firebase.storage()
 
 
 @app.route('/')
